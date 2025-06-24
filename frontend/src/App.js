7e173c2d888c660dffd3b4086880637fd20904e0
Tristan Tan <tristantan0899@gmail.com>
2025-06-25 03:01:54 +0800

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -54,11 +28,6 @@ function App() {
         {/* Add more routes here as needed */}
       </Routes>
     </Router>
-    // <div className="App">
-    //   <header className="App-header"> 
-        
-    //   </header>
-    // </div>
   );
 }
 export default App;
